4ce9ab252c791cd7399d53ba0791fd649793d025
juny0901 <dlrhkdwns12@gmail.com>
2022-10-27 11:20:09 +0900

    2일차 강의실 변경

--- src/main/java/kh/study/test/Test11.java
+++ src/main/java/kh/study/test/Test11.java
@@ -3,6 +3,6 @@ package kh.study.test;
 public class Test11 {
 	public static void main(String[] args) {
 		System.out.println("집에서 작업");
-		System.out.println("2일차");
+		System.out.println("2일차강의실");
 	}
 }
